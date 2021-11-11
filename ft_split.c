ebec634d9025cb08df37af2063ff881213ac0ee2
Abdeljalil Ait omar <aait-oma@e2r8p8.1337.ma>
2021-11-11 14:01:38 +0100

    finally

--- ft_split.c
+++ ft_split.c
@@ -6,40 +6,103 @@
 /*   By: aait-oma <aait-oma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 09:24:06 by aait-oma          #+#    #+#             */
-/*   Updated: 2021/11/09 15:38:58 by aait-oma         ###   ########.fr       */
+/*   Updated: 2021/11/11 12:26:22 by aait-oma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
+#include "libft.h"
 
-// char	**ft_split(char const *s, char c)
-// {
-// 	int		i;
-// 	int		j;
-// 	int		k;
-// 	char	**array;
+static size_t	ft_wordcount(const char *str, char c)
+{
+	size_t	i;
+	size_t	count;
 
-// 	i = 0;
-// 	j = 0;
-// 	while (s[i])
-// 	{
-// 		if (s[i] == c)
-// 			j++;
-// 		i++;
-// 	}
-// 	array = (char **) malloc(sizeof(char *) * (j + 1));
-// 	i = 0;
-// 	while (i < (j + 1))
-// 	{
-// 		k = 0;
-// 		while (s[j] != c)
-// 			k++;
-// 		k++;
-// 		*array = (char *) malloc(sizeof(char) * k);
-// 		ft_memcpy(*array, s, (k - 1));
-// 		array[k] = '\0';
-// 		s += k;
-// 		array++;
-// 		i++;
-// 	}	
-// }
+	i = 0;
+	count = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		while (str[i] == c && str[i])
+			i++;
+		while (str[i] != c && str[i])
+			i++;
+		count++;
+	}
+	if (str[i - 1] == c)
+		count--;
+	return (count);
+}
+
+static size_t	ft_wordlen(const char *str, char c)
+{
+	size_t	len;
+
+	len = 0;
+	while (*str != c && *str)
+	{
+		str++;
+		len++;
+	}
+	return (len);
+}
+
+static char	**ft_clean(char **ptr, size_t count)
+{
+	size_t	i;
+
+	if (ptr)
+	{
+		i = 0;
+		while (i < count)
+		{
+			if (ptr[i])
+				free(ptr[i]);
+			i++;
+		}
+		free(ptr);
+	}
+	return (NULL);
+}
+
+static char	**copyover(char **output, const char *s, char c)
+{
+	size_t	j;
+	size_t	k;
+
+	j = 0;
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		if (*s == '\0')
+			continue ;
+		output[j] = malloc(sizeof(char *) * ft_wordlen(s, c) + 1);
+		if (output[j] == NULL)
+			return (ft_clean(output, j));
+		k = 0;
+		while (*s != c && *s)
+		{
+			output[j][k] = *s;
+			k++;
+			s++;
+		}
+		output[j][k] = '\0';
+		j++;
+	}
+	output[j] = NULL;
+	return (output);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	char	**output;
+
+	if (s == NULL)
+		return (NULL);
+	output = malloc(sizeof(char *) * ft_wordcount(s, c) + 1);
+	if (output == NULL)
+		return (NULL);
+	copyover(output, s, c);
+	return (output);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aait-oma <aait-oma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 12:12:35 by aait-oma          #+#    #+#             */
-/*   Updated: 2021/11/10 14:20:50 by aait-oma         ###   ########.fr       */
+/*   Updated: 2021/11/11 12:37:06 by aait-oma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,17 +54,16 @@ void	ft_putnbr_fd(int n, int fd);
 
 typedef struct s_list
 {
-    void            *content;
-    struct s_list   *next;
-    
-}   t_list;
-t_list   *ft_lstnew(void *content);
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
-int	ft_lstsize(t_list *lst);
-t_list  *ft_lstlast(t_list *lst);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
-void    ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
-void    ft_lstiter(t_list *lst, void (*f)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
